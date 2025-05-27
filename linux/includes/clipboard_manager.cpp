d32b91374c986e220a818ce22dfb157cdbc2d59e
Karan Yadav <xpresskaran98@gmail.com>
2025-05-27 20:38:27 +0530

    add clipboard manager class
    Signed-off-by: Karan Yadav <xpresskaran98@gmail.com>

---
+++ linux/includes/clipboard_manager.cpp
@@ -0,0 +1,31 @@
+#include "clipboard_manager.hpp"
+
+ClipboardManager::~ClipboardManager() { stop(); }
+
+std::string ClipboardManager::get_text() const {
+  std::string str;
+  if (clip::has(clip::text_format())) {
+    clip::get_text(str);
+  }
+  return str;
+}
+
+void ClipboardManager::set_text(const std::string &str) { clip::set_text(str); }
+
+void ClipboardManager::poll(std::function<void(const std::string &)> cb) {
+  std::thread([this, cb]() {
+    while (running_) {
+      std::string clipboard_content = get_text();
+
+      if (clipboard_content != last_clipboard_content_) {
+        std::cout << "Syncing..." << std::endl;
+        last_clipboard_content_ = clipboard_content;
+        cb(clipboard_content);
+      }
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  }).detach();
+}
+
+void ClipboardManager::stop() { running_ = false; }
---
+++ linux/includes/clipboard_manager.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <atomic>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <thread>
+
+#include "clip.h"
+
+class ClipboardManager {
+  std::string last_clipboard_content_;
+  std::atomic<bool> running_{true};
+
+public:
+  ClipboardManager() = default;
+  ~ClipboardManager();
+
+  std::string get_text() const;
+  void set_text(const std::string &str);
+  void poll(std::function<void(const std::string &)> cb);
+  void stop();
+};
